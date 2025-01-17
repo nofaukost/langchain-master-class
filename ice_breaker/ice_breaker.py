709f15061e7d5b436d508a604a8d836ba56d855b
xfactor-toml <xfactor.toml@gmail.com>
2025-01-17 01:53:11 +0000

    feat: enhance ice_breaker functionality with LinkedIn summary generation
    - Implemented a summary generation feature using LangChain with OpenAI and Ollama models.
    - Added a prompt template to create a summary and interesting facts based on provided LinkedIn information.
    - Updated the main entry point to invoke the summary generation and print results.

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,6 +1,33 @@
-import os
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+
+information = """
+    Name: John Doe
+    Position: Software Engineer
+    Company: Google
+    Education: Bachelor of Science in Computer Science
+    Skills: Python, JavaScript, React, Node.js
+"""
 
 if __name__ == "__main__":
-    print("Hello, LangChain!")
+    summary_template = """
+        given the Linkedin information {information} about a person I want you to create:
+        1. A short summary
+        2. two interesting facts about them
+    """
     
-    print(os.environ['OPENAI_API_KEY'])
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    
+    llm_openai = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    chain_openai = summary_prompt_template | llm_openai | StrOutputParser()
+    res_openai = chain_openai.invoke({"information": information})
+    
+    print(res_openai)
+    
+    llm_ollama = ChatOllama(model="llama3.2")
+    chain_ollama = summary_prompt_template | llm_ollama | StrOutputParser()
+    res_ollama = chain_ollama.invoke({"information": information})
+    
+    print(res_ollama)
