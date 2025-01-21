e77c25a64fdcd58207b768641e38225c124da79b
xfactor-toml <xfactor.toml@gmail.com>
2025-01-21 19:41:08 +0000

    refactor: update ice_breaker.py to improve summary generation process
    - Commented out the StrOutputParser and integrated summary_parser for output formatting.
    - Enhanced the summary_prompt_template to include format instructions dynamically.
    - Cleaned up imports by removing unused StrOutputParser and adding summary_parser for better clarity and maintainability.

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,11 +1,11 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 # from langchain_ollama import ChatOllama
-from langchain_core.output_parsers import StrOutputParser
+# from langchain_core.output_parsers import StrOutputParser
 from third_parties.linkedin import scrape_linkedin_profile
 
 from agents.linkedin_lookup import linkedin_lookup_agent
-
+from output_parsers import summary_parser
 from dotenv import load_dotenv
 
 def ice_breaker_with_name(name: str) -> str:
@@ -16,12 +16,17 @@ def ice_breaker_with_name(name: str) -> str:
         given the Linkedin information {information} about a person I want you to create:
         1. A short summary
         2. two interesting facts about them
+        \n{format_instructions}
     """
     
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
+    )
     
     llm_openai = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
-    chain_openai = summary_prompt_template | llm_openai | StrOutputParser()
+    # chain_openai = summary_prompt_template | llm_openai | StrOutputParser()
+    chain_openai = summary_prompt_template | llm_openai | summary_parser
     res_openai = chain_openai.invoke({"information": linkedin_data})
     
     # llm_ollama = ChatOllama(model="llama3.2")
